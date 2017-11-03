acd1aece0e0b32fd161286186609b9b33e775449
Vasu Gupta <androfly.official@gmail.com>
2017-11-03 19:20:39 +0530

    Numpy Work

--- Advanced Operations/indexing_with_boolean_arrays.py
+++ Advanced Operations/indexing_with_boolean_arrays.py
@@ -4,3 +4,9 @@ a1 = np.arange(12).reshape(3,4)
 a2 = a1 > 4
 
 print(a2)
+print("-------Getting only elements that are true-----------")
+print(a1[a2])
+
+print("------------Replacing true numbers by -1 -------------")
+a1[a2] = -1
+print(a1)
