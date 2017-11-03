ac558a7f2d4289d68dd9e66e6f9f1a5901057298
Vasu Gupta <androfly.official@gmail.com>
2017-11-03 19:03:39 +0530

    Numpy Work

--- Advanced Operations/indexing_slicing.py
+++ Advanced Operations/indexing_slicing.py
@@ -7,3 +7,6 @@ print("1D array indexing : " , a1[0:2])
 print("2D array Element : " , a2[1,2]) # Returns that particular element i.e. element present at 1,2
 print("2D array indexing 1 : \n" , a2[0:2]) # Gives rows from 0 to 1
 print("2D array indexing 2 : \n" , a2[0:2,2]) # Gives rows from 0 to 1 with column index 2
+print("2D array indexing 3 : \n" , a2[-1]) # Gives last row
+print("2D array indexing 4 : \n" , a2[:,-1]) # Gives last column
+print("2D array indexing 5 : \n" , a2[-1 , -1]) # Gives intersection of last row and last column
