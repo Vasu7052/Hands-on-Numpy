e13540da1268c9d82021528994158076c3e7e1d8
Vasu Gupta <androfly.official@gmail.com>
2017-11-03 19:13:45 +0530

    Numpy Work

--- Advanced Operations/stacking_two_arrays.py
+++ Advanced Operations/stacking_two_arrays.py
@@ -1,4 +1,22 @@
 import numpy as np
 
-a1 = np.array([5,2,7,54,3,5])
-a2 = np.array([[1,2,3],[4,5,6],[7,8,9]])
+a1 = np.arange(6).reshape(3,2)
+a2 = np.arange(6,12).reshape(3,2)
+
+print("Vertical Stacking : \n" , np.vstack((a1,a2)))
+print("------------------------------")
+print("Horizontal Stacking : \n" , np.hstack((a1,a2)))
+
+print("---------Original Array------------")
+a3 =np.arange(30).reshape(2,15)
+print(a3)
+result = np.hsplit(a3,3)
+print("---------Splitting------------")
+print(result)
+print("---------result 0 ---------------------")
+print(result[0])
+print("---------result 1 ---------------------")
+print(result[1])
+print("---------result 2 ---------------------")
+print(result[2])
+
