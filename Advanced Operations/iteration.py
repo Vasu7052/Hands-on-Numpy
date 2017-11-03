ce94dfc29c5c8f7d2eaeb3bebd37a1ab5c613a75
Vasu Gupta <androfly.official@gmail.com>
2017-11-03 19:06:08 +0530

    Numpy Work

--- Advanced Operations/iteration.py
+++ Advanced Operations/iteration.py
@@ -1,4 +1,12 @@
 import numpy as np
 
-a1 = np.array([5,2,7,54,3,5])
+a1 = np.array([5,2,7,4,3])
 a2 = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+for row in a2 :
+    print(row)
+
+print("----Flat Array-----")
+
+for cell in a2.flat :
+    print(cell)
