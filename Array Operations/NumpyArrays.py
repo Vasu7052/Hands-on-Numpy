b6915c23011813d139c2a8aea880f995b859d165
Vasu Gupta <androfly.official@gmail.com>
2017-11-03 18:30:35 +0530

    Numpy Work

--- Array Operations/NumpyArrays.py
+++ Array Operations/NumpyArrays.py
@@ -3,7 +3,7 @@ import numpy as np
 a1 = np.array([5,2,7,54,3,5])
 
 a2 = np.array([[1,2,3],[4,5,6],[7,8,9]])
-print("Dimension : " + str(a2.ndim) + "\nItem Size : " + str(a2.itemsize) + "\nDataType : " + str(a2.dtype))
+print("Dimension : " + str(a2.ndim) + "\nItem Size : " + str(a2.itemsize) + "\nDataType : " + str(a2.dtype) + "\nSize : " + str(a2.size) + "\nShape : " + str(a2.shape))                                       
 
 a3 = np.array([[1,2,3],[4,5,6],[7,8,9]] , dtype=np.float64)
 print("-----------------------\nDimension : " + str(a3.ndim) + "\nItem Size : " + str(a3.itemsize) + "\nDataType : " + str(a3.dtype))
