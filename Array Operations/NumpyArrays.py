516fb6ce4782af30aa154e555071ceb915819a4a
Vasu Gupta <androfly.official@gmail.com>
2017-11-03 18:29:03 +0530

    Numpy Work

--- Array Operations/NumpyArrays.py
+++ Array Operations/NumpyArrays.py
@@ -3,5 +3,14 @@ import numpy as np
 a1 = np.array([5,2,7,54,3,5])
 
 a2 = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print("Dimension : " + str(a2.ndim) + "\nItem Size : " + str(a2.itemsize) + "\nDataType : " + str(a2.dtype))
 
-print(a2.ndim)
+a3 = np.array([[1,2,3],[4,5,6],[7,8,9]] , dtype=np.float64)
+print("-----------------------\nDimension : " + str(a3.ndim) + "\nItem Size : " + str(a3.itemsize) + "\nDataType : " + str(a3.dtype))
+
+
+
+
+
+
+
